2e583b41d9833e565675a9631a67149bd1b2b9a9
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-08-03 16:31:27 -0400

    lay fetch

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,25 @@
 import React, {useState, useEffect} from 'react'
 
-
+const apiKEY = process.env.REACT_APP_NASA_API_KEY
+const APIlink5 = `https://api.nasa.gov/insight_weather/?api_key=${apiKEY}&feedtype=json&ver=1.0`
 
 function Weather() {
     
-    
+    const [weather, setWeather] = useState({data: []})
+
+    useEffect(() => {
+        fetch(APIlink5)
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setWeather({data: result})
+            }
+        )
+        .catch(error => console.log(error))
+    }, [])
+
+
+    console.log(weather.data[592])
     
     return (
         <div>
