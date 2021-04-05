6f1cdb5fea9cae374f7956f74f5225c4147f1ae3
SJellen <sjellen@me.com>
2021-04-05 15:18:13 -0400

    clean up on code

--- src/Curiosity.js
+++ src/Curiosity.js
@@ -13,7 +13,6 @@ export default function Curiosity() {
     const [today, setToday] = useState()
     const [newDate, setNewDate] = useState("2020-01-01")
 
-
     useEffect(() => {
         fetch(APIlink2)
         .then(res => res.json())
@@ -27,8 +26,6 @@ export default function Curiosity() {
         .catch(error => console.log(error))
     }, [])
 
-
-
     function newRequest(newDate){
         const newDateLink = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${newDate}&api_key=${apiKEY}`
         fetch(newDateLink)
@@ -36,14 +33,10 @@ export default function Curiosity() {
         .then(
             (result) => {
                 setRoverPhotos({data: result.photos})
-
             }
-        )
-        
+        ) 
     }
-
   
-       
     function handleImageSubmit(event) {
         event.preventDefault()
     }
@@ -51,7 +44,6 @@ export default function Curiosity() {
     function handleImageChange(event) {
         setAmountShown(event.target.value)
     }
-
 
 
     function HandelDatePicker(date) {
@@ -64,16 +56,14 @@ export default function Curiosity() {
     }
 
     
-     let min = new Date('2012-08-05')
-     const minDate = min.setDate(min.getDate() + 2)
-     let max = new Date(today)
-     const maxDate = max.setDate(max.getDate() + 1)
-
+    let min = new Date('2012-08-05')
+    const minDate = min.setDate(min.getDate() + 2)
+    let max = new Date(today)
+    const maxDate = max.setDate(max.getDate() + 1)
 
     let slice = Object.entries(roverPhotos.data).slice(0,amountShown).map(entry => entry[1])
     
     const photoMap = slice.map((i) => (
-       
         <img src={i.img_src} alt="mars rover" key={i.id}/>
     ))
 
@@ -84,53 +74,49 @@ export default function Curiosity() {
         return convertDate.toLocaleString('en-US', options)
     }
 
-
-
     return (
         <div className="rover-container" id="curiosity">
-                <h1 className="section-title">Mars Rover: Curiosity</h1>
+            <h1 className="section-title">Mars Rover: Curiosity</h1>
                 <div className="mission-box">
                     <h3>Start of Mission</h3>
-                            <p>Launch date: <span className="launch">November 26th, 2011</span><br></br>Landing Date: <span className="launch">August 6th, 2012</span></p>
-                
+                        <p>Launch date: <span className="launch">November 26th, 2011</span><br></br>Landing Date: <span className="launch">August 6th, 2012</span></p>
                 </div>
                
             <div className="select-box">
                 <div className="search-box-rover">
-            <DatePicker
-                    dateFormat="yyyy/MM/DD" 
-                    name="newDate"
-                    onChange={HandelDatePicker}
-                    maxDate={maxDate}
-                    minDate={minDate}
-                    placeholderText="Select a day &nbsp;  &nbsp;  &nbsp; &#128269;"
-                    showYearDropdown
-                    yearDropdownItemNumber={40}
-                    scrollableYearDropdown
-                    className="date-picker"
-            /> 
-            </div>
+                    <DatePicker
+                        dateFormat="yyyy/MM/DD" 
+                        name="newDate"
+                        onChange={HandelDatePicker}
+                        maxDate={maxDate}
+                        minDate={minDate}
+                        placeholderText="Select a day &nbsp;  &nbsp;  &nbsp; &#128269;"
+                        showYearDropdown
+                        yearDropdownItemNumber={40}
+                        scrollableYearDropdown
+                        className="date-picker"
+                    /> 
+                </div>
         
-            <span className="current-selected-date">{dateConversion(visibleDate)}</span>
+                <span className="current-selected-date">{dateConversion(visibleDate)}</span>
 
-        <form className="select-box-rover" onSubmit={handleImageSubmit}>
-        <label className="image-label">Number of Images</label>
-        <select value={amountShown} onChange={handleImageChange}>
-            
-            <option value="12">12</option>
-            <option value="25">25</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-            <option value="200">200</option>
-            <option value="500">All</option>
-        </select>
-        </form>
+            <form className="select-box-rover" onSubmit={handleImageSubmit}>
+                <label className="image-label">Number of Images</label>
+                <select value={amountShown} onChange={handleImageChange}>
+                <option value="12">12</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+                <option value="200">200</option>
+                <option value="500">All</option>
+                </select>
+            </form>
             </div>
             
-        <div className="rover-photo-box">
+            <div className="rover-photo-box">
             {photoMap}
-        </div>
-        <a href="https://www.jpl.nasa.gov/missions/mars-science-laboratory-curiosity-rover-msl/" target="_blank" rel="noopener noreferrer" className="material-icons exit-to-rover">exit_to_app_rounded_icon</a>
+            </div>
+            <a href="https://www.jpl.nasa.gov/missions/mars-science-laboratory-curiosity-rover-msl/" target="_blank" rel="noopener noreferrer" className="material-icons exit-to-rover">exit_to_app_rounded_icon</a>
         </div>
     )
 }
